ab2f25662caba95d168e6f5050ac47bf9deb5fa5
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-05-02 16:06:33 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
-int factorial(int);
+int factorial(int);//function declaration
 int main()
 {int a;
-cout<<"Enter the Number:";
+cout<<"Enter the Number:";//entering a number whose factorial to be found
 cin>>a;
-int k= factorial(a);
+int k= factorial(a);//function call
 cout<<"\nFactorial = "<<k;
 return 0;
 }
-int factorial(int y)
+int factorial(int y)//function definition
 {
 if(y==0)
 return 1;
 else
-return y*factorial(y-1);
+return y*factorial(y-1);//recursion
 }
