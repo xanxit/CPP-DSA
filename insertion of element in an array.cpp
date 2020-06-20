89f24887e27dbd4895c0538fec5bb85587b9b0e8
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-20 11:04:08 +0530

    Update insertion of element in an array.cpp

--- insertion of element in an array.cpp
+++ insertion of element in an array.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,insert,pos;
+  cout<<"Enter the number of element you want in your array\n";
+  cin>>n;
+  int a[n];//creating an array of the given size
+  for(i=0;i<n;i++)
+  {
+    cout<<"Enter the Element in "<<i<<" index : ";
+    cin>>a[i];
+    cout<<endl;
+  }
+  cout<<"Enter the element you want to insert : ";
+  cin>>insert;
+  cout<<endl;
+  cout<<"Enter the position you want to add : ";
+  cin>>pos;
+  cout<<endl;
+   n++;
+  for(i=n;i>=pos;i--)
+    a[i]=a[i-1];
+  a[pos-1]=insert;
+  for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+  return 0;
+}
