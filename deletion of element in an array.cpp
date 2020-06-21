8019a735bb95fd3cdd2cc46b2869a1440eb04a35
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-21 12:09:05 +0530

    If the position is known

--- deletion of element in an array.cpp
+++ deletion of element in an array.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[]={2,3,5,7,0};
+    int i,pos;
+    cout<<"Enter the position of the element to be deleted ";
+    cin>>pos;
+    for(i =pos;i<5;i++)
+    {
+        a[i]=a[i+1];
+    }
+    for(i=0;i<4;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
