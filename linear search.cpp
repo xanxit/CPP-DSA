95b35e88df42a364d34548b94033eec620e3d8a4
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-23 11:24:11 +0530

    Update linear search.cpp

--- linear search.cpp
+++ linear search.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[]={2,3,5,7,0};
+    int i,k;
+    cout<<"Enter the element to be searched ";
+    cin>>k;
+    for(i =0;i<5;i++)
+    {
+        if(k==a[i])
+        cout<<endl<<"The position of the element in the array is "<<i<<endl;
+    }
+    return 0;
+}
