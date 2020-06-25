0db75cce36f00cf051616051783b951db00421c0
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-25 22:55:11 +0530

    Update check if array is sorted.cpp

--- check if array is sorted.cpp
+++ check if array is sorted.cpp
@@ -1,1 +1,22 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,d=0;
+    int a[]={1,2,9,4,5};
+    for(i=0;i<4;i++)
+    {
+        if(a[i]<=a[i+1])
+        continue;
+        else
+        {
+            d++;
+            break;
+        }
+    }
+    if(d==0)
+    cout<<"SORTED";
+    else if (d==1)
+    cout<<"UNSORTED";
+    return 0;
+}
