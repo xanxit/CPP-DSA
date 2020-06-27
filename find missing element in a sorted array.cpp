b1c071c3159c5af7c22e269acea3e32f94ab41c4
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-27 21:26:00 +0530

    Update find missing element in a sorted array.cpp

--- find missing element in a sorted array.cpp
+++ find missing element in a sorted array.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int a[]={1,3,6};
+    for(i=0;i<6;i++)
+   {
+       if(a[i]-i==a[0])
+       continue;
+       else
+       {
+           int temp=a[0]+i;
+           a[i+1]=a[i];
+           a[i]=temp;
+           cout<<"Missing elements at position "<<i+1<<" is "<<a[i]<<" \n";
+       }
+   }
+   cout<<"\nThe Whole Array looks like: ";
+   for(i=0;i<6;i++)
+   cout<<a[i]<<" ";
+  
+    return 0;
+}
