1b67a788e863140637c16c3191a794668e57c9f8
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-07-02 11:50:47 +0530

    Create Length of the string.cpp

---
+++ Length of the string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+  char str[]="Sanchit";
+  int len=0;
+  for(int i=0;str[i]!=0;i++)
+  {
+    len++;
+  }
+  cout<<len;
+  return 0;
+ 
+}
