f58010c7345d184420e1f41f8bc44f07276bf262
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-07-02 12:00:10 +0530

    Update Reverse a string.cpp

--- Reverse a string.cpp
+++ Reverse a string.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+  char str[]="Sanchit";
+  int len=0;
+  for(int i=0,j=6;i<j;i++,j--)
+  {
+    len=str[i];
+    str[i]=str[j];
+    str[j]=len;
 
+  }
+  for(int i=0;str[i]!=0;i++)
+  cout<<str[i];
+  return 0;
+ 
+}
