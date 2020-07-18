8edc793ec084c8ba2a0d990998f5fc9e675679af
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-07-18 18:51:44 +0530

    Update print diagonal matrix.cpp

--- print diagonal matrix.cpp
+++ print diagonal matrix.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+  int a[3][3],x=1;
+  for(int i=0;i<3;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      if(i==j){
+      cout<<x<<" ";
+      x++;
+      }
+      else 
+      cout<<"0 ";
+    
+    }
+    cout<<"\n";
+  }
+  return 0;
+}
