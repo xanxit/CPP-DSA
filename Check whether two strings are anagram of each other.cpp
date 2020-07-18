87475db31f755bb74c42e5dd9942bfce4b752dbe
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-07-18 18:07:53 +0530

    Update Check whether two strings are anagram of each other.cpp

--- Check whether two strings are anagram of each other.cpp
+++ Check whether two strings are anagram of each other.cpp
@@ -1,1 +1,27 @@
 #include<iostream>
+using namespace std;
+int main()
+{
+char a[]="decimal";
+char b[]="medical";
+int c=0;
+for(int i=0;a[i]!=0;i++)
+{
+  for(int j=0;b[j]!=0;j++)
+  {
+    if(a[i]==b[j])
+    {
+      c++;
+      break;
+    }
+    else continue;
+  }
+}
+if(c==7)//7 is the length of the string
+{
+  cout<<"Anagram";
+}
+else
+cout<<"no";
+return 0;
+}
