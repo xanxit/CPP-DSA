f9204e9a4fd1c5ea9071ab566c1aa4fbedec0c84
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-07-26 16:27:13 +0530

    Update count the nodes of a linked list.cpp

--- count the nodes of a linked list.cpp
+++ count the nodes of a linked list.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+using namespace std;
+class node
+{
+  public:
+  int data;
+  node*next;
+};
+//node*first=NULL;
+void insert(node*,int,int);
+void disp(node*);
+int main()
+{
+  /*int i,x,n;
+  cout<<"Enter no. of nodes ";
+  cin>>n;
+  for(i=0;i<n;i++)
+  {
+    cout<<"Enter data ";
+    cin>>x;
+    insert(first,i,x);
+  }*/
+  node*head=NULL;
+  node*second=NULL;
+  node*third=NULL;
+  node*last=NULL;
+  head=new node;
+  second = new node;
+  third=new node;
+  last=new node;
+  head->data=1;
+  head->next=second;
+  second->data=0;
+  second->next=third;
+  third->data=9;
+  third->next=last;
+  last->data=6;
+  last->next=NULL;
 
+
+  disp(head);
+  return 0;
+}
+/*void insert(node*p,int pos,int val)
+{
+  node*t=NULL;
+  t=new node;
+  if(pos==0)
+  {
+    t->data=val;
+    t->next=first;
+    first=t;
+  }
+  else
+  {
+    p=first;
+    for(int i=0;i<pos-1;i++)
+    {
+      p=p->next;
+    }
+    if(p!=0)
+    {
+      t->data=val;
+      t->next=p->next;
+      p->next=t;
+    }
+  }
+}*/
+void disp(node*p)
+{int count=0;
+  while(p!=NULL)
+  {
+    count++;
+    p=p->next;
+  }
+  cout<<"The number of nodes created : "<<count ;
+}
