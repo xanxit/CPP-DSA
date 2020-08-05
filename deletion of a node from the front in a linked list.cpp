5efbffc33db5fd7dd19c07e164e0026becbb4913
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-08-05 09:05:48 +0530

    Update deletion of a node from the front in a linked list.cpp

--- deletion of a node from the front in a linked list.cpp
+++ deletion of a node from the front in a linked list.cpp
@@ -1,1 +1,32 @@
-
+#include<iostream>
+using namespace std;
+class Node
+{
+  public:
+  int data;
+  Node*next;
+};
+Node*first=NULL;
+void insert(Node*p,int val,int pos)
+{
+Node*t=new Node;
+if(pos==0)
+{
+  t->data=val;
+  t->next=first;
+  first=t;
+}
+else
+{
+p=first;
+for(int i=0;i<pos-1;i++)
+{
+   p=p->next;
+}
+if(p!=NULL)
+{
+  t->data=val;
+  t->next=p->next;
+  p->next=t;
+}
+}
