af1be2b14ea1757ea6e070e53f515a16ee0e6a44
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-08-07 22:34:08 +0530

    Update delete a node from end.cpp

--- delete a node from end.cpp
+++ delete a node from end.cpp
@@ -1,1 +1,72 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+class node{
+  public:
+  int data;
+  node*next;
+};
+node*first=NULL;
+void push(node**p,int n)
+{
+  node*t=new node;
+  t->data=n;
+  t->next=first;
+  first=t;
+
+}
+void add(node**p,int k)
+{
+  node*temp=*p;
+  node*t=new node;
+  t->data=k;
+  t->next=NULL;
+  while(temp->next!=NULL)
+    temp=temp->next;
+  temp->next=t;
+}
+void deletefromend()
+{
+  node*temp=first;
+  node*prev;
+  while(temp->next!=NULL)
+  {
+    prev=temp;
+    temp=temp->next;
+  }
+  prev->next=NULL;
+  free(temp);
+}
+
+void display(node*p)
+{cout<<"LINKED LIST:\n";
+  while(p!=NULL)
+  {
+    cout<<p->data<<" ";
+    p=p->next;
+  }
+}
+int main()
+{
+  int p,l,i=0;
+  cout<<"Enter the value in the first node: ";
+  cin>>p;
+  push(&first,p);
+  char n[3];
+  while(i>=0){
+  cout<<"Do you want to add a new node :\nEnter Yes or No :";
+  cin>>n;
+  if(strcmp(n,"yes")==0||strcmp(n,"Yes")==0){
+  cout<<"Enter value for the next node:";
+  cin>>l;
+  add(&first,l);
+  }
+  else {
+  cout<<"Dhanyawaad aapka\n";
+  break;
+  }
+  }
+  deletefromend();
+  display(first);
+}
 
