a208bb910a1b8df470ead49636206c0f75690b8a
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-08-23 15:48:19 +0530

    Update reversing a doubly linked list using tail ptr.cpp

--- reversing a doubly linked list using tail ptr.cpp
+++ reversing a doubly linked list using tail ptr.cpp
@@ -1,1 +1,58 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class node
+{
+  public:
+  int data;
+  node*next;
+  node*prev;
+}*head=NULL,*tail=NULL;
+void node1(int n)
+{
+  node *t=new node;
+  t->data=n;
+  t->next=NULL;
+  t->prev=NULL;
+  head=tail=t;
+}
+void othernodes(int n)
+{
+  node *t=new node;
+  t->data=n;
+  tail->next=t;
+  t->prev=tail;
+  tail=t;
 
+}
+void display()
+{
+  while(tail!=NULL)
+  {
+    cout<<tail->data<<" ";
+    tail=tail->prev;
+  }
+}
+int main()
+{
+  int p,l,i=0;
+  cout<<"Enter the value in the first node: ";
+  cin>>p;
+  node1(p);
+  char n[3];
+  while(i>=0){
+  cout<<"Do you want to add a new node :\nEnter Yes or No :";
+  cin>>n;
+  if(strcmp(n,"yes")==0||strcmp(n,"Yes")==0){
+  cout<<"Enter value for the next node:";
+  cin>>l;
+  othernodes(l);
+  }
+  else {
+  cout<<"Dhanyawaad aapka\n";
+  break;
+  }
+  }
+  display();
+
+}
