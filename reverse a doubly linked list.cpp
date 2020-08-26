5c8caa0f93aaef24eb9fa290f1b1d16db44f9445
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-08-26 15:26:48 +0530

    Update reverse a doubly linked list.cpp

--- reverse a doubly linked list.cpp
+++ reverse a doubly linked list.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class node
+{
+  public:
+  int data;
+  node*next;
+  node*prev;
+}*head=NULL,*tail=NULL;
+void node1(int n)
+{
+  node *t=new node;
+  t->data=n;
+  t->next=NULL;
+  t->prev=NULL;
+  head=tail=t;
+}
+void othernodes(int n)
+{
+  node *t=new node;
+  t->data=n;
+  tail->next=t;
+  t->prev=tail;
+  tail=t;
 
+}
