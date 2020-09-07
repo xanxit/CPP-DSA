21bdcc4364b7cf73b7c7ea82da786af8a406bb82
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-09-07 09:22:14 +0530

    Update implementing stack using array.cpp

--- implementing stack using array.cpp
+++ implementing stack using array.cpp
@@ -1,1 +1,63 @@
-
+#include <iostream>
+#include<string.h>
+using namespace std;
+class stack
+{
+  public:
+  int size,top,*s;
+};
+void createstack(stack *st)
+{ 
+  cout<<"Enter Size of the stack : ";
+  cin>>st->size;
+  st->s=new int[st->size];
+  st->top=-1;
+ 
+}
+void push(stack *st,int val)
+{
+  if(st->top==st->size-1)
+  {
+    cout<<"OVERFLOW";
+  }
+  else
+  {
+    st->top++;
+    st->s[st->top]=val;
+  }
+}
+void printstack(stack st)
+{
+  for(int i=0;i<st.size;i++)
+  {
+    cout<<st.s[i]<<endl;
+  }
+}
+int main()
+{ char n[3];
+  stack st;
+  int i=1,l,u;
+  createstack(&st);
+  cout<<"Enter first element in the stack : ";
+  cin>>u;
+  push(&st,u);
+  while(i<=st.size-1)
+  {
+  cout<<"DO YOU WANT TO PUSH AN ELEMENT IN THE STACK?\nYES OR NO\nCHOICE =";
+  cin>>n;
+  if(strcmp(n,"yes")==0||strcmp(n,"Yes")==0)
+  {
+  cout<<"Enter value for the next node:";
+  cin>>l;
+  push(&st,l);
+  }
+  else
+ {
+  cout<<"Dhanyawaad aapka\n";
+  break;
+  }
+  i++;
+}
+  printstack(st);
+  return 0;
+}
